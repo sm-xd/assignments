9ff3d1901829bfeb2ec50b1b4f2de4a3a9124f29
Sky <126008711+sumitmishra01@users.noreply.github.com>
2024-07-11 20:25:01 +0530

    week 1 tasks

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,15 @@
 
 function isAnagram(str1, str2) {
 
+  let l1 =str1.length;
+  let l2 =str2.length;
+
+  if(l1!=l2) return false;
+
+  let sort1 = str1.toLowerCase().split("").sort().join("");
+  let sort2 = str2.toLowerCase().split("").sort().join("");
+  
+  return sort1===sort2;
 }
 
 module.exports = isAnagram;
